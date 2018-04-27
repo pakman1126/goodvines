08176d4e2f0399d59bf1adc87c01a458197d68c6
John Pak <pakman1126@yahoo.com>
2018-04-27 12:20:13 -0700

    added price field to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426184203) do
+ActiveRecord::Schema.define(version: 20180427175347) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180426184203) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "price"
   end
 
 end
