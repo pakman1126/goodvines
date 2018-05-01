22e5c6af6b9cb8e78989c5bba09d26aa465ffff8
John Pak <pakman1126@yahoo.com>
2018-05-01 10:01:43 -0700

    Create orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   root 'simple_pages#index'
 
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
