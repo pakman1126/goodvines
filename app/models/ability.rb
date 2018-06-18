0b9b3cad205470821bdcd770e380a4aef9c0b592
John Pak <pakman1126@yahoo.com>
2018-06-18 15:53:29 -0700

    Moderating User Comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,11 +7,11 @@ class Ability
     can :manage, User, id: user.id
     #
     #  user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+      if user.admin?
+          can :manage, :all
+      else
+          can :read, :all
+      end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
