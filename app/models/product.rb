af7b3e59742da0b6d7185eb26ad37b5267fc0fe5
John Pak <pakman1126@yahoo.com>
2018-06-18 13:19:19 -0700

    Comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  has_many :comments
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
 end
